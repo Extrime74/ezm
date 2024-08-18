9f8eebff77209b10386305aca1b262ed3259f3fd
Alexander Posukhovskiy <extrime74@gmail.com>
2024-08-18 18:31:54 +0300

    Model "Client" added

---
+++ app/models/client.rb
@@ -0,0 +1,6 @@
+class Client < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :phone_number, presence: true, numericality: { only_integer: true }
+
+end
