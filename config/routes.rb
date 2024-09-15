cce44e553f84b3c24833ffd0814936cb530492bb
Alexander Posukhovskiy <extrime74@gmail.com>
2024-09-15 18:34:49 +0300

    Minor changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   resources :orders
   resources :clients do
-    resources :orders, only: [:clients_orders] # Или другой метод, который нужен
+    resources :orders, only: [:clients_orders]
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :clients, only: [:new, :create]
-  
+
   get 'clients/:client_id/orders', to: 'orders#clients_orders', as: 'client_orders'
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
